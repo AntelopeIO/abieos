16067902ed1ed86d6a93716449da818e6ef7f483
Eric Passmore <eric.passmore@gmail.com>
2023-01-19 03:14:12 -0800

    added json from hex

--- src/util_generate_json_from_hex.cpp
+++ src/util_generate_json_from_hex.cpp
@@ -1,5 +1,5 @@
 //
-// Created by Eric Passmore on 1/14/23.
+// Created by Eric Passmore on 1/19/23.
 // Purpose: command line option to generate hex code from JSON
 //   may be used to generate deserialization tests cases in other languages and external packages
 //
@@ -290,19 +290,18 @@ uint64_t get_contract_id(ABI_CONTRACT abiContractName) {
 // Main work done here four steps
 //      1) create empty context
 //      2) set the context
-//      3) parse json to binary
-//      4) create hex from bin
+//      4) create json from hex
 // abiContractName: enum with contract names
 // schema: the name of the data type or reference to schema in the ABI contract
-// json: the name and values
+// hex: hex encoded schema
 // verbose: flag to print out step by step messages
-std::string generate_hex_from_json(ABI_CONTRACT abiContractName, const char *schema, const char *json, bool verbose) {
-    if (verbose) fprintf(stderr, "Schema is: %s and json is %s\n\n",schema,json);
+std::string generate_json_from_hex(ABI_CONTRACT abiContractName, const char *schema, const char *hex, bool verbose) {
+    if (verbose) fprintf(stderr, "Schema is: %s and hex is %s\n\n",schema,hex);
 
     // create empty context
     abieos_context_s *context = abieos_create();
     if (! context) throw std::runtime_error("unable to create context");
-    if (verbose) fprintf(stderr,"step 1 of 4: created empty ABI context\n");
+    if (verbose) fprintf(stderr,"step 1 of 3: created empty ABI context\n");
 
     // set the transaction contexts. set for each type we support.
     bool successTransactionAbi = abieos_set_abi(context, 0, transactionAbi);
@@ -310,28 +309,19 @@ std::string generate_hex_from_json(ABI_CONTRACT abiContractName, const char *sch
     // state history abi defined in ship.abi.cpp
     bool successStateHistoryAbi = abieos_set_abi(context, 2, state_history_plugin_abi);
     if (! (successTransactionAbi && successPackedTransactionAbi && successStateHistoryAbi )) {
-        throw std::runtime_error(abieos_get_error(context));
+        throw std::runtime_error("unable to set context");
     }
-    if (verbose) fprintf(stderr,"step 2 of 4: established context for transactions, packed transactions, and state history\n");
+    if (verbose) fprintf(stderr,"step 2 of 3: established context for transactions, packed transactions, and state history\n");
 
-    // convert from json to binary. binary stored with context
-    // get contract id returns integer for the ABI contract we passed in by name
-    bool successJsonToBin = abieos_json_to_bin_reorderable(
+    // convert hex to json
+    std::string json = abieos_hex_to_json(
         context,
         get_contract_id(abiContractName),
         schema,
-        json
-        );
-    if (!successJsonToBin) {
-        fprintf(stderr,"failed in step 3: using context %iu\n", abiContractName);
-        throw std::runtime_error(abieos_get_error(context));
-    }
-    if (verbose) fprintf(stderr,"step 3 of 4: completed parsing json to binary\n");
-
-    // now time to return the hex string
-    std::string hex = abieos_get_bin_hex(context);
-    if (verbose) fprintf(stderr,"step 4 of 4: converted binary to hex\n\n");
-    return hex;
+        hex
+    );
+    if (verbose) fprintf(stderr,"step 3 of 3: converted hex to json\n\n");
+    return json;
 }
 
 // prints usage
@@ -341,7 +331,7 @@ void help(const char* exec_name) {
     fprintf(stderr, "\t-p abi packed transaction\n");
     fprintf(stderr, "\t-s state history\n");
     fprintf(stderr, "\t-v verbose, print out steps\n");
-    fprintf(stderr,"\t-j json: string to convert to hex\n");
+    fprintf(stderr,"\t-h hex: string to convert to json\n");
     fprintf(stderr,"\t-x type: a specific data type or schema section (example uint16, action, name, uint8[])\n");
     fprintf(stderr, "\texample: generate_hex_from_json -x bool -j true\n\n");
 }
@@ -355,13 +345,13 @@ int main(int argc, char *argv[]) {
     int opt;
 
     try {
-        while ((opt = getopt(argc, argv, "tpsvj:x:")) != -1) {
+        while ((opt = getopt(argc, argv, "tpsvh:x:")) != -1) {
             switch (opt) {
             case 't': abiContractName = TRANSACT_ABI; break;
             case 'p': abiContractName = PACKED_TRANSACTION_ABI; break;
             case 's': abiContractName = STATE_HISTORY_ABI; break;
             case 'v': verbose = true; break;
-            case 'j': json = optarg; break;
+            case 'h': hex = optarg; break;
             case 'x': type = optarg; break;
             case '?':
                 if ('c' == optopt) {
@@ -376,21 +366,21 @@ int main(int argc, char *argv[]) {
         }
 
         // check for required params
-        if (json.empty() || type.empty()) {
+        if (hex.empty() || type.empty()) {
             help(*argv);
             exit(EXIT_FAILURE);
         }
 
-        hex = generate_hex_from_json(
+        json = generate_json_from_hex(
             abiContractName,
             type.c_str(),
-            json.c_str(),
+            hex.c_str(),
             verbose
-            );
-        if (hex.length() > 0) {
-            printf("%s\n",hex.c_str());
+        );
+        if (json.length() > 0) {
+            printf("%s\n",json.c_str());
         } else {
-            fprintf(stderr,"no hex value\n");
+            fprintf(stderr,"no json value\n");
         }
         return 0;
     } catch (std::exception& e) {
